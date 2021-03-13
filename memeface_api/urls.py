d13417b442b90353f0e083879898abb2c261c2ec
Andrey Belskiy <diksandab@gmail.com>
2021-03-13 08:40:47 +0600

    Added images download support

--- memeface_api/urls.py
+++ memeface_api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import MemeViewSet, RatingViewSet, UserViewSet
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet)
@@ -9,4 +12,5 @@ router.register('ratings', RatingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
