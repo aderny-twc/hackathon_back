fea51b0a7b2c0c58bc750694ed3f890373cb55ab
Andrey Belskiy <diksandab@gmail.com>
2021-03-15 16:23:31 +0600

    Added description of models. Added DB description. Excluded migration files.

--- memeface_api/models.py
+++ memeface_api/models.py
@@ -5,16 +5,24 @@ from django.db.models import Sum
 
 
 class Meme(models.Model):
+    """
+    Content model.
+    Contains information about the user and content.
+    """
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=400, blank=True)
-    image = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Image', blank=True)
-    user = models.ForeignKey(User, verbose_name="Author", on_delete=models.CASCADE, blank=True)
+    image = models.ImageField(upload_to='photos/%Y/%m/%d/',
+                                verbose_name='Image',
+                                blank=True)
+    user = models.ForeignKey(User,
+                            verbose_name="Author",
+                            on_delete=models.CASCADE,
+                            blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
 
     def like_rating(self):
-        # ratings = Rating.objects.filter(meme=self)
-        ratings = Rating.objects.filter(meme=self).aggregate(Sum('like'))['like__sum']
-        return ratings
+        ratings = Rating.objects.filter(meme=self).aggregate(Sum('like'))
+        return ratings['like__sum']
 
     def avg_rating(self):
         sum = 0
@@ -36,18 +44,22 @@ class Meme(models.Model):
 
 
 class Rating(models.Model):
+    """
+    User rating model.
+    The m2m table contains information about the content ID
+    and rating of a particular user. 
+    """
     meme = models.ForeignKey(Meme, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    like = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(1)], default=0, blank=False)
+    like = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(1)],
+                                default=0,
+                                blank=False)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
 
     class Meta:
         unique_together = (('user', 'meme'),)
         index_together = (('user', 'meme'),)
-
-    class Meta:
         verbose_name = 'Rating'
         verbose_name_plural = "Ratings"
         ordering = ['-created_at']
-
 
